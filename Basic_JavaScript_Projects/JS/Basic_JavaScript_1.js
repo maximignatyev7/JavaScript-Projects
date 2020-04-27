b0875907688e7327756f3d959097ef95951b92fd
Max <maximignatyev7@gmail.com>
2020-04-26 23:21:44 -0700

    Basic_JavaScript_Projects
    Refresh Basic_JavaScript_Projects folder

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,23 @@
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+    Daughter = "Penny", Son = "Zorro";
+var Family = Family.fontcolor("red");
+var Dad = Dad.fontcolor("purple");
+var Mom = Mom.fontcolor("yellow");
+var Daughter = Daughter.fontcolor("orange");
+var Son = Son.fontcolor("green");
+document.write(Son);
+
+document.write(3+3);
+
+window.alert("Hello, World!")
+
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+function My_First_Function() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
